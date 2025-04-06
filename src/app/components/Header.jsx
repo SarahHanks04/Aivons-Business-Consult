7acc0804651f6429957fad23e6f9d69371daadcd
SarahHanks04 <sarahhanks04@gmail.com>
2025-04-06 23:48:31 +0100

    Update Header component: change Case Shop link from /faq to /new-case

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
     { name: "Home", href: "/" },
     { name: "Services", href: "/services", hasDropdown: true },
     { name: "Pages", href: "/experience" },
-    { name: "Case Shop", href: "/faq" },
+    { name: "Case Shop", href: "/new-case" },
     { name: "Blog", href: "/blog" },
     { name: "Content", href: "/content" },
   ];
