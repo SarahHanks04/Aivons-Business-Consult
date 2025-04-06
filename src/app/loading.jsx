cb8f3bd86d46abd2baa52751dd88dd6ba65315c6
SarahHanks04 <sarahhanks04@gmail.com>
2025-04-07 00:18:03 +0100

    Remove loading text and update loading animation styles in Loading component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -6,15 +6,15 @@ export default function Loading() {
       {/* <span className="text-xl font-light text-gray-800">Loading</span> */}
       <div className="flex space-x-1">
         <div
-          className="w-2 h-2 bg-blue-500 rounded-full animate-bounce"
+          className="w-2 h-2 bg-blue rounded-full animate-bounce"
           style={{ animationDelay: "0ms" }}
         />
         <div
-          className="w-2 h-2 bg-blue-500 rounded-full animate-bounce"
+          className="w-2 h-2 bg-blue rounded-full animate-bounce"
           style={{ animationDelay: "150ms" }}
         />
         <div
-          className="w-2 h-2 bg-blue-500 rounded-full animate-bounce"
+          className="w-2 h-2 bg-blue rounded-full animate-bounce"
           style={{ animationDelay: "300ms" }}
         />
       </div>
