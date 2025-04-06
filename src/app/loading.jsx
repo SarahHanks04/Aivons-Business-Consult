0a5b293a3d2ce2bbd508578c3e612ab081e9f2b4
SarahHanks04 <sarahhanks04@gmail.com>
2025-04-07 00:17:29 +0100

    Remove loading text from Loading component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@
 export default function Loading() {
   return (
     <div className="flex space-x-1 items-center">
-      <span className="text-xl font-light text-gray-800">Loading</span>
+      {/* <span className="text-xl font-light text-gray-800">Loading</span> */}
       <div className="flex space-x-1">
         <div
           className="w-2 h-2 bg-blue-500 rounded-full animate-bounce"
